a6e78231caef9b78d5c78becab1afed80027481c
Constantin Raducanu <raducanu.costi@gmail.com>
2021-05-30 20:01:40 +0300

    Force export_dashboards.sh to write the dashboard locally
    Signed-off-by: Constantin Raducanu <raducanu.costi@gmail.com>

--- grafana-provisioning/export_dashboards.sh
+++ grafana-provisioning/export_dashboards.sh
@@ -11,7 +11,7 @@ set -o nounset
 
 echo "Exporting Grafana dashboards from $HOST"
 for dash in $(curl -s "$FULLURL/api/search?query=&" | jq -r '.[] | select(.type == "dash-db") | .uid'); do
-        curl -s "$FULLURL/api/dashboards/uid/$dash" | jq -r . > dashboards/${dash}.json
-        slug=$(cat dashboards/${dash}.json | jq -r '.meta.slug')
-        mv dashboards/${dash}.json dashboards/${dash}-${slug}.json
+        curl -s "$FULLURL/api/dashboards/uid/$dash" | jq -r . > ${dash}.json
+        slug=$(${dash}.json | jq -r '.meta.slug')
+        mv ${dash}.json ${dash}-${slug}.json
 done
